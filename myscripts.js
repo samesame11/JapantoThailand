d8e5680d20edf096533bd6e075976979f7791210
Chalisa Changmai <47394405+samesame11@users.noreply.github.com>
2020-10-05 05:47:19 +0700

    add chat icon
    Add chat icon

--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,9 @@
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-    interval: false
-  });
-  
-  // for every slide in carousel, copy the next slide's item in the slide.
-  // Do the same for the next, next item.
-  $('.multi-item-carousel .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-  });
+document.querySelector(".ChatBtn").addEventListener("click", openForm);
+document.querySelector(".close").addEventListener("click", closeForm);
+function openForm() {
+  document.querySelector(".openChat").style.display = "block";
+  document.querySelector("#textChat").focus();
+}
+function closeForm() {
+  document.querySelector(".openChat").style.display = "none";
+}
